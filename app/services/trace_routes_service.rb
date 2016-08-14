fa4e45e68a74e73a68aeda35504680d7179a7ea7
Wagner Bigal <wagnerbigal@gmail.com>
2016-08-14 19:47:29 -0300

    Refatorando a TraceRoutesService

--- app/services/trace_routes_service.rb
+++ app/services/trace_routes_service.rb
@@ -1,13 +1,22 @@
 class TraceRoutesService
   class << self
-    def to_default_destination client_id, lat_origin, lng_origin
-      origin = "#{lat_origin},#{lng_origin}"
-      destination = Way.default_destination
-      response = GoogleMapsDirectionsApiClient.get origin, destination
-      way = WayAdapter.convert_from_google_json(response)
-      way.client_id = client_id
-      way.save!
-      way
+    def to_default_destination client_id, lat_origin, lng_origin, query_id = nil
+      get_way(client_id, lat_origin, lng_origin, query_id)
     end
+
+    private
+      def get_route client_id, lat_origin, lng_origin, query_id = nil
+        origin = "#{lat_origin},#{lng_origin}"
+        destination = Way.default_destination
+        GoogleMapsDirectionsApiClient.get origin, destination
+      end
+
+      def get_way client_id, lat_origin, lng_origin, query_id = nil
+        response = get_route(client_id, lat_origin, lng_origin, query_id)
+        way = WayAdapter.convert_from_google_json(response)
+        way.client_id = client_id
+        way.save!
+        way
+      end
   end
 end
