d6115fb45af23d84ec77b19df7091b882326f1d6
Wagner Bigal <wagnerbigal@gmail.com>
2016-08-15 23:04:05 -0300

    Configurando mock syntax do rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,6 +40,7 @@ RSpec.configure do |config|
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
+    mocks.syntax = :should
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
