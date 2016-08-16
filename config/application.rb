f46d1bcaa1e96705b9aec5ca85dfd1273537d3c5
Wagner Bigal <wagnerbigal@gmail.com>
2016-08-15 22:42:48 -0300

    Ajustando site para pt-BR e fuso de Brasília

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,10 @@ module MelhorCaminho
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Brasilia'
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.available_locales = [ 'pt-BR' ]
   end
 end
